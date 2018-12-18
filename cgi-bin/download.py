b92df4ddbbf07763d134be81297e188752d713eb
lightslife <guangxuhuang@163.com>
2018-12-19 00:27:21 +0800

    init

---
+++ cgi-bin/download.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# HTTP 头部
+print "Content-Disposition: attachment; filename=\"foo.txt\"";
+print
+# 打开文件
+fo = open("foo.txt", "rb")
+
+str = fo.read();
+print str
+
+# 关闭文件
+fo.close()
